193dae727f5b5a10e7b9141476e9e65879939ad2
kstasiak98 <53605181+kstasiak98@users.noreply.github.com>
2020-06-14 18:48:42 +0200

    add dist

--- mysqlconnection/core/views.py
+++ mysqlconnection/core/views.py
@@ -7,7 +7,7 @@ from django.conf import settings
 class IndexTemplateView(LoginRequiredMixin, TemplateView):
 
     def get_template_names(self):
-        if not settings.DEBUG:
+        if settings.DEBUG:
             template_name = "index-dev.html"
         else:
             template_name = "index.html"
--- mysqlconnection/mysqlconnection/settings.py
+++ mysqlconnection/mysqlconnection/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hvj)la!#wnor&$co0-wwn4@&qi@rj#bylm)6l$6^4oii-^@hpn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
